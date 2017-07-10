578624f453c8bc9f64c973f9513785de42360cc0
MNovokhovsky <M.Novokhovsky@gmail.com>
2017-07-09 23:28:19 -0500

    Create map.js for education locations

---
+++ map.js
@@ -0,0 +1,31 @@
+function initMap() {
+
+    var locations = [
+
+      ['Illinois Institute of Technology', 41.83826, -87.62902, 1],
+
+      ['Art Center College of Design', 34.1689, 118.1852, 2],
+
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+
+      zoom: 11,
+
+      center: new google.maps.LatLng(41.83826, -87.62902),
+
+    });
+
+    for (var i = 0; i < locations.length; i++) { 
+
+      var marker = new google.maps.Marker({
+
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+
+        map: map
+
+      });
+
+    }
+
+}
